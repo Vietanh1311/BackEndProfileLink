f129afa5d3536e23d55a5545e3b0bb885d43c2e0
Vietanh1311 <83020392+Vietanh1311@users.noreply.github.com>
2023-01-13 13:52:54 +0700

    start

---
+++ ProfileLinks/src/main/java/com/example/profilelinks/ProfileLinksApplication.java
@@ -0,0 +1,13 @@
+package com.example.profilelinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileLinksApplication.class, args);
+	}
+
+}
